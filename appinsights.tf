2b8f6b7edeabce2709252d60691060143e45cd85
Tim Jacomb <21194782+timja@users.noreply.github.com>
2023-08-17 13:39:23 +0100

    Update appinsights.tf

--- appinsights.tf
+++ appinsights.tf
@@ -19,7 +19,7 @@ output "appInsightsInstrumentationKey" {
   sensitive = true
 }
 
-resource "azurerm_key_vault_secret" "app_insights_key" {
+resource "azurerm_key_vault_secret" "connection_string" {
   name         = "app-insights-connection-string"
   value        = azurerm_application_insights.appinsights.connection_string
   key_vault_id = module.vault.key_vault_id
