5977390c52c5d4d140ad4a9918b95309eb2a1bd4
Tim Jacomb <21194782+timja@users.noreply.github.com>
2023-08-17 13:37:03 +0100

    Store app insights connection string

--- appinsights.tf
+++ appinsights.tf
@@ -20,6 +20,12 @@ output "appInsightsInstrumentationKey" {
 }
 
 resource "azurerm_key_vault_secret" "app_insights_key" {
+  name         = "app-insights-connection-string"
+  value        = azurerm_application_insights.appinsights.connection_string
+  key_vault_id = module.vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "app_insights_key" {
   name         = "AppInsightsInstrumentationKey"
   value        = azurerm_application_insights.appinsights.instrumentation_key
   key_vault_id = module.vault.key_vault_id
