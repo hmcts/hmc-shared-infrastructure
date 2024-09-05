11735c1ad6b6ebed8d787d9a2b00ccd49aa12377
Dan Lysiak <50049163+danlysiak@users.noreply.github.com>
2024-09-05 09:18:34 +0100

    Update service-bus.tf

--- service-bus.tf
+++ service-bus.tf
@@ -13,7 +13,7 @@ module "servicebus-namespace" {
 }
 
 module "servicebus-queue-request" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
+  source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=4.x"
   name                = "${var.product}-to-hmi-${var.env}"
   namespace_name      = module.servicebus-namespace.name
   resource_group_name = azurerm_resource_group.rg.name
@@ -22,7 +22,7 @@ module "servicebus-queue-request" {
 }
 
 module "servicebus-queue-response" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
+  source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=4.x"
   name                = "${var.product}-from-hmi-${var.env}"
   namespace_name      = module.servicebus-namespace.name
   resource_group_name = azurerm_resource_group.rg.name
@@ -53,7 +53,7 @@ resource "azurerm_key_vault_secret" "servicebus_primary_shared_access_key" {
 }
 
 module "servicebus-topic" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-topic?ref=master"
+  source              = "git@github.com:hmcts/terraform-module-servicebus-topic?ref=4.x"
   name                = "${var.product}-to-cft-${var.env}"
   namespace_name      = module.servicebus-namespace.name
   resource_group_name = azurerm_resource_group.rg.name
@@ -62,11 +62,10 @@ module "servicebus-topic" {
 }
 
 module "servicebus-subscription" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
+  source              = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=4.x"
   name                = "${var.product}-subs-to-cft-${var.env}"
-  namespace_name      = module.servicebus-namespace.name
+  namespace_id        = module.servicebus-namespace.id
   topic_name          = module.servicebus-topic.name
-  resource_group_name = azurerm_resource_group.rg.name
 
   depends_on = [module.servicebus-topic]
 }
